2f18f50e6d4ebb551430c3869c2bf5c7081e1260
Eric Jin <ericjin2@illinois.edu>
2019-11-23 17:29:21 -0600

    Merge branch 'master' of https://github.com/EricJ19/CS125_Map_Info
    # Conflicts:
    #	.idea/vcs.xml
    
    Many untested changes

--- app/src/main/java/com/example/cs125finalproject/Player.java
+++ app/src/main/java/com/example/cs125finalproject/Player.java
@@ -1,6 +1,9 @@
 package com.example.cs125finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,16 @@ public class Player extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game_activity);
         updatePlayerScoresUI(NewGameActivity.currentScore, NewGameActivity.previousCurrentScore);
+        final Intent intent = new Intent(this, MainActivity.class);
+        Button goBackToMainMenu = findViewById(R.id.goBackToMainMenu);
+        goBackToMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                // Change the label's text
+                startActivity(intent);
+            }
+        });
+
     }
     private String[] playerLast9ScoresStr = new String[9];
     public void updatePlayerScoresUI(String currentScore, String previousCurrentScore) {
