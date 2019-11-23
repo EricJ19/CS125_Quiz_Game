86ea46d3fe47cf577c6556560554ea49a7901c9c
Eric Jin <ericjin2@illinois.edu>
2019-11-23 00:45:47 -0600

    Merge branch 'master' of https://github.com/EricJ19/CS125_Map_Info
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/cs125finalproject/NewGameActivity.java
+++ app/src/main/java/com/example/cs125finalproject/NewGameActivity.java
@@ -1,4 +1,32 @@
 package com.example.cs125finalproject;
 
+/**
+ * Activity of the game after main menu and shows the randomly generated questions page.
+ */
 public class NewGameActivity {
+    /**
+     * Generates the random questions.
+     */
+    public void questionGenerate() {
+
+    }
+    /**
+     * Generates the random answers.
+     */
+    public void answersGenerate() {
+
+    }
+    /**
+     * Update questions and answers when an answer is clicked.
+     */
+    public void onClickAnswer() {
+
+    }
+    /**
+     * Updates the score part of UI after the answer is clicked by user.
+     */
+    public void updatePlayerScore() {
+
+    }
+
 }
