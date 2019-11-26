be2186da8deb3c3304dacf3a769356e598efda85
ethansc2 <ethansc2@illinois.edu>
2019-11-26 17:27:53 -0600

    Version 1 (start on MainActivity and Player classes)

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.cs125finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerSetUp();
 
+    }
+    /** creates spinner to hold topic choices and sets on choice listeners. */
+    private void spinnerSetUp() {
+        Spinner spinner = findViewById(R.id.gameTopic);
+        List<String> list = new ArrayList<>();
+        list.add("topic 1");
+        list.add("topic 2");
+        list.add("topic 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+        AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
+        spinner.setOnItemSelectedListener(listener);
     }
 }
--- app/src/main/java/com/example/cs125finalproject/Player.java
+++ app/src/main/java/com/example/cs125finalproject/Player.java
@@ -1,4 +1,25 @@
 package com.example.cs125finalproject;
 
 public class Player {
+    /** the players name. */
+    private String name;
+    /** the amount of points the player got. */
+    private int points;
+    /** the constructor to create new player. */
+    public Player(String setName, int setPoints) {
+        name = setName;
+        points = setPoints;
+    }
+    /** return player name. */
+    public String getName() {
+        return name;
+    }
+    /** returns amount of points gotten by player. */
+    public int getPoints() {
+        return points;
+    }
+    /** set the amount of points gotten by player. */
+    public void setPoints(int setPoints) {
+        points = setPoints;
+    }
 }
